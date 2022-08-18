97f210c2af4e94d88c5e2aefda7eae442518b509
LudvigThunberg <ludvigthunberg@gmail.com>
2022-08-18 11:05:52 +0200

    added GET/booking/bookings

--- backend/routers/bookingRouter.js
+++ backend/routers/bookingRouter.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const bookingRouter = express.Router();
 
-const BookingRouter = require("../models/BookingModel.js");
+const BookingModel = require("../models/BookingModel.js");
+
+bookingRouter.get("/bookings", async (req,res)=>{
+
+    try{
+        let bookings = await BookingModel.find({date : req.body.date})
+        let bookingSeating = bookings.filter((booking)=> bookings.seating === req.body.seating)
+        if(bookingSeating.length > 14){
+            res.send("already booked")
+        }else {
+            res.send("booking is possible")
+        }
+    }catch(error) {
+        res.send("hello you cunt!")
+    }
+})
 
 module.exports = bookingRouter;
