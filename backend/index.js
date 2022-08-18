5186831a9877220770c9fae5f6869743aa0657a2
Johan Elgström <johan.elgstrom@gmail.com>
2022-08-18 11:17:06 +0200

    added POST for booking

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,21 @@ const BookingModel = require("./models/BookingModel.js");
 const CustomerModel = require("./models/CustomerModel.js");
 
 const app = express();
-
-app.use(express.static("public"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.post("/new-booking", async (req, res) => {
+  const newBooking = new BookingModel({
+    date: req.body.date || Date.now(), //Date.now() tillfällig tills react calendar skickar via bodyn
+    seating: req.body.seating,
+    tableamount: req.body.tableamount || 1, //default 1 bord för G
+  });
+  await newBooking.save();
+
+  res.sendStatus(201);
+});
 
 app.listen(8000, () => {
-  console.log("Server is running");
+  console.log("Server is running at http://localhost:8000 " + Date.now());
 });
--- backend/models/BookingModel.js
+++ backend/models/BookingModel.js
@@ -1,12 +1,12 @@
-const { Schema, model } = require("mongoose");
+const { model, Schema } = require("mongoose");
 
 const bookingSchema = new Schema({
-  date: { type: Date, required: true },
-  seating: { type: Number, required: true },
-  tableamount: { type: Number, required: true },
-  customer: { type: Schema.Types.ObjectId, ref: "Customers", required: true },
+  date: { type: Date, required: true }, //Date kommer hämtas från react calendar
+  seating: { type: Number, required: true }, // 1 är 18:00, 2 är 21:00
+  tableamount: { type: Number, required: true }, //antal bord
+  customer: { type: Schema.Types.ObjectId, ref: "Customers" }, //lägg in <required: true> vid senare tillfälle
 });
 
 const BookingModel = model("Bookings", bookingSchema);
 
-module.export = BookingModel;
+module.exports = BookingModel;
