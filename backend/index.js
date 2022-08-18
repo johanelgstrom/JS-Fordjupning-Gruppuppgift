d10c49a6eeaf7f0276ea73e248679e1f198b1095
LudvigThunberg <ludvigthunberg@gmail.com>
2022-08-18 13:59:45 +0200

    GET Works

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const CustomerModel = require("./models/CustomerModel.js");
 
 //ROUTER REQUIREMENTS
 const bookingRouter = require("./routers/bookingRouter.js");
-
+ 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
