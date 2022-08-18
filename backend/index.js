c8fa2ef2d444770e677be9e5f95ee1b0d85f4a01
LudvigThunberg <ludvigthunberg@gmail.com>
2022-08-18 10:17:03 +0200

    commit

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const BookingModel = require("./models/BookingModel.js");
 const CustomerModel = require("./models/CustomerModel.js");
 
 const app = express();
-
+//comment
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
