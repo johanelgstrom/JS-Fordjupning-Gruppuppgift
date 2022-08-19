810e6f98473faccbfcf0e15af2f049d0b4530b8e
Johan Elgström <johan.elgstrom@gmail.com>
2022-08-19 10:33:26 +0200

    added style to start, links in menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { Home } from "./components/pages/Home";
+import { Book } from "./components/pages/Book";
+import { Contact } from "./components/pages/Contact";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />}></Route>
+            <Route path="/book" element={<Book />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
