5f5736cca96d2352c4c2eaa71f2338721f85fa6c
LudvigThunberg <ludvigthunberg@gmail.com>
2022-08-19 11:04:07 +0200

    added calendar started with layout

---
+++ src/components/pages/Booking.tsx
@@ -0,0 +1,14 @@
+import Calendar from "react-calendar";
+
+export const Booking = () => {
+  return (
+    <>
+      <main>
+        <h3>Boka bord</h3>
+        <form>
+          <Calendar />
+        </form>
+      </main>
+    </>
+  );
+};
